dcbdcea90e7ebdea8561e743fab04165f08982c4
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2021-01-14 17:00:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext import CommandHandler, MessageHandler, Filters, Updater, CallbackQueryHandler
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater, CallbackQueryHandler, messages
 from telegram import ChatAction, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 import os
 import img2pdf
